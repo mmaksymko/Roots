438a72ace300e18b96d48d92a325ce1a1e841bd4
OMartsin <100160685+OMartsin@users.noreply.github.com>
2023-02-04 19:45:06 +0200

    init

--- R00T/js/plugins.js
+++ R00T/js/plugins.js
@@ -5,5 +5,6 @@ var $plugins =
 {"name":"EliMZ_Book","status":true,"description":"♦5.3.0♦ Essential plugin for all Eli plugins.","parameters":{"engine":"{\"pixelPerfect\":\"false\",\"disableEffekseer\":\"false\",\"styleOverflow\":\"false\"}","playtest":"{\"openDevTools\":\"false\",\"nwWindowPos\":\"0, 0\",\"--- MZ ONLY ---\":\"\",\"gameFocus\":\"false\",\"quickRestart\":\"true\"}"}},
 {"name":"AltMenuScreen","status":true,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"EliMZ_MoveChoices","status":true,"description":"♦5.0.0♦ Change the position of the Choice Window.","parameters":{"okDelay":"0"}},
-{"name":"Cae_DashOpts","status":true,"description":"v1.0 - Dash options: speed, frame-skip, event dashing, etc.","parameters":{"Dash Extra Speed":"0.00","Skip Standing Frame":"false","Add Save Data":"false","--- Advanced ---":"","Property: dashSpeed":"_dashSpeed","Property: isDashing":"_isDashing","Property: Save":"Cae_DashOpts"}}
+{"name":"Cae_DashOpts","status":true,"description":"v1.0 - Dash options: speed, frame-skip, event dashing, etc.","parameters":{"Dash Extra Speed":"0.00","Skip Standing Frame":"false","Add Save Data":"false","--- Advanced ---":"","Property: dashSpeed":"_dashSpeed","Property: isDashing":"_isDashing","Property: Save":"Cae_DashOpts"}},
+{"name":"Tile Changer (MV)","status":true,"description":"Change tiles on map or copy tiles from another map","parameters":{}}
 ];
