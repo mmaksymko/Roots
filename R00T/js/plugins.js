f7871149edcdb0bc35d3aab79508ae7d80cfa6b8
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-02-06 03:02:21 +0200

    RELEASE v1.0

--- R00T/js/plugins.js
+++ R00T/js/plugins.js
@@ -6,5 +6,5 @@ var $plugins =
 {"name":"AltMenuScreen","status":true,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"EliMZ_MoveChoices","status":true,"description":"♦5.0.0♦ Change the position of the Choice Window.","parameters":{"okDelay":"0"}},
 {"name":"Cae_DashOpts","status":true,"description":"v1.0 - Dash options: speed, frame-skip, event dashing, etc.","parameters":{"Dash Extra Speed":"0.00","Skip Standing Frame":"false","Add Save Data":"false","--- Advanced ---":"","Property: dashSpeed":"_dashSpeed","Property: isDashing":"_isDashing","Property: Save":"Cae_DashOpts"}},
-{"name":"Tile Changer (MV)","status":true,"description":"Change tiles on map or copy tiles from another map","parameters":{}}
+{"name":"HIME_TimedChoiceSelection","status":true,"description":"v1.1 - Set a timer that automatically selects a choice for the\r\nplayer when the timer expires.","parameters":{}}
 ];
